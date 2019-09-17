59f5e7055763a30b71f407a7aea0d08c1fa48d81
Elbio <elbio.caetano@hotmail.com>
2019-09-16 21:52:42 -0300

    Showing customized logo image

--- src/components/Main/constants/index.tsx
+++ src/components/Main/constants/index.tsx
@@ -1,6 +1,8 @@
+import LogoSystem from "../../../assets/images/Logoccb.png";
+
 export const SYSTEM = {
   module: "CAGEF",
   name: "Cadastro Geral de Funções",
-  image: "",
+  image: LogoSystem,
   version: "1.0.0"
 };
--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,8 +1,9 @@
+import { MagaSideBar } from "maga-components";
 import * as React from "react";
-import { MagaNav, Divider, Grid, Responsive, MagaSideBar } from "maga-components";
-import { userData, isAdmin } from '../../config/Utils';
-import { SYSTEM } from "./constants";
+import Notifications from "react-notify-toast";
+import { isAdmin, userData } from '../../config/Utils';
 import history from "../../routes/history";
+import { SYSTEM } from "./constants";
 interface Props {
   children: any;
 }
@@ -17,95 +18,72 @@ export default class Main extends React.Component<Props> {
 
   render() {
     const { children } = this.props;
-    const { width } = this.state;
     return (
-      <Responsive fireOnMount onUpdate={this.handleOnUpdate}>
-        <div className="page">
-          <Grid>
-            <Grid.Column color="blue" width={width > 1700 ? 2 : 3}>
-              <MagaSideBar system={SYSTEM}
-              onSelectSideMenu={(route: any) => {
-                history.push(`${process.env.PUBLIC_URL}${route}`);
-              }}
-              onSelectDropDownUserMenu={(item: any) => {
-                history.push(`${process.env.PUBLIC_URL}${item.route}`);
-              }}
-              onSelectMessage={(item: any) => {
-                console.log(item);
-              }}
-              onLogout={() => {
-                window.location.href = `${process.env.REACT_APP_PUBLIC_URL}`;
-              }}
-              onSelectLogo={() => {
-                window.location.href = `${process.env.REACT_APP_PUBLIC_URL}`;
-              }}
-                menu={[
-                  {
-                    title: "Home",
-                    icon: "home",
-                    route: "/"
-                  },
-                  {
-                    name: "Cadastros",
-                    icon: "wpforms",
-                    ref: "registration",
-                    route: "/cadastros",
-                    subItens: [{
-                      title: "Casas de Oração",
-                      ref: "prayingHouses",
-                      route: "/cadastros/casas-oracao"
-                    }, {
-                      title: "Voluntários",
-                      ref: "volunteers",
-                      route: "/cadastros/voluntarios"
-                    }, {
-                      ...(isAdmin() && {
-                        title: "Cidades",
-                        ref: "cities",
-                        route: "/cadastros/cidades"
-                      })
-                    }, {
-                      ...(isAdmin() && {
-                        title: "Ministérios / Cargos",
-                        ref: "ministeriesOrPositions",
-                        route: "/cadastros/ministerios-cargos"
-                      })
-                    }, {
-                      ...(isAdmin() && {
-                        title: "Usuários",
-                        ref: "users",
-                        route: "/cadastros/usuarios"
-                      })
-                    }
-                    ]
-                  }
-                ]}
-              />
-            </Grid.Column>
-            <Grid.Column
-              width={width > 1700 ? 14 : 13}
-              style={{ paddingLeft: 0 }}
-            >
-              <MagaNav
-                logo="magalog"
-                urlExit="/login"
-                urlLogo="/"
-                userName={userData().userName}
-                fluid
-                menu={[]}
-              />
-              <div>
-              </div>
-              <Grid padded>
-                <Grid.Column width={16}>
-                  <Divider hidden />
-                  {children}
-                </Grid.Column>
-              </Grid>
-            </Grid.Column>
-          </Grid>
-        </div>
-      </Responsive>
+
+      <React.Fragment>
+        <MagaSideBar system={SYSTEM}
+          activeUser={userData().userName}
+          onSelectSideMenu={(route: any) => {
+            history.push(`${process.env.PUBLIC_URL}${route}`);
+          }}
+          onSelectDropDownUserMenu={(item: any) => {
+            history.push(`${process.env.PUBLIC_URL}${item.route}`);
+          }}
+          onSelectMessage={(item: any) => {
+            console.log(item);
+          }}
+          onLogout={() => {
+            window.location.href = `${process.env.REACT_APP_PUBLIC_URL}/login`;
+          }}
+          onSelectLogo={() => {
+            window.location.href = `${process.env.REACT_APP_PUBLIC_URL}`;
+          }}
+          menu={[
+            {
+              title: "Home",
+              icon: "home",
+              route: "/"
+            },
+            {
+              title: "Cadastros",
+              icon: "wpforms",
+              ref: "registration",
+              route: "/cadastros",
+              subItems: [{
+                title: "Casas de Oração",
+                ref: "prayingHouses",
+                route: "/cadastros/casas-oracao"
+              }, {
+                title: "Voluntários",
+                ref: "volunteers",
+                route: "/cadastros/voluntarios"
+              }, {
+                ...(isAdmin() && {
+                  title: "Cidades",
+                  ref: "cities",
+                  route: "/cadastros/cidades"
+                })
+              }, {
+                ...(isAdmin() && {
+                  title: "Ministérios / Cargos",
+                  ref: "ministeriesOrPositions",
+                  route: "/cadastros/ministerios-cargos"
+                })
+              }, {
+                ...(isAdmin() && {
+                  title: "Usuários",
+                  ref: "users",
+                  route: "/cadastros/usuarios"
+                })
+              }
+              ]
+            }
+          ]}
+        >
+          {children}
+        </MagaSideBar>}
+        <Notifications />
+      </React.Fragment>
     );
   }
 }
