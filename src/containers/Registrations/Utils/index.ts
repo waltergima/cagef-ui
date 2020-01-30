d4e56f11d3f5c3f6d52545d455b3c2018723c8b9
Elbio <elbio.caetano@hotmail.com>
2020-01-30 00:06:17 -0300

    Fixing sorting

--- src/containers/Registrations/Utils/index.ts
+++ src/containers/Registrations/Utils/index.ts
@@ -1,10 +1,15 @@
 import queryString from "query-string";
+import { getSorted } from "maga-components";
 
 export const parseParams = (value: any) => {
     let filtered: any = {};
     value.filtered.forEach((v: any) => {
         filtered[v.id] = v.value;
     });
+    if (value.sorted && value.sorted.length) {
+        filtered['orderBy'] = value.sorted[0].id;
+        filtered['direction'] = value.sorted[0].desc ? 'desc' : 'asc';
+    }
     const params = {
         offset: value.offset / value.limit,
         limit: value.limit,
